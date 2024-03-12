54bc9b7fd2c18cafc0f7cdba34a4f36a05955d5d
putbullet <soulaimanettabaas@gmail.com>
2024-03-12 07:11:17 +0000

    o

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,11 +9,11 @@
  * Return: If parent is NULL or an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
  *
- * Description: If parent already has a left-child, the new node
- *              takes its place and the old left-child is set as
- *              the left-child of the new node.
+ * Description: If parent already has a riht-child, the new node
+ *              takes its place and the old right-child is set as
+ *              the right-child of the new node.
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
